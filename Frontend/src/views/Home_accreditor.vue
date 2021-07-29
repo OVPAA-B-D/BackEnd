d54b5c7970be2d4bb4ddd7fa4358457a560a31f6
Kaezuuu <86968215+Kaezuuu@users.noreply.github.com>
2021-07-29 22:42:45 +0800

    Update Home_accreditor.vue

--- Frontend/src/views/Home_accreditor.vue
+++ Frontend/src/views/Home_accreditor.vue
@@ -50,7 +50,9 @@
       <div class="mt-10 pb-0 cursor-wait">
         <h1 class="text-yellow-150 text-5xl">Information Technology</h1>
         <h5 class="text-blue-150 text-3xl">Chairman in charge</h5>
-          </span>
+         <span class="flex justify-start text-xs text-yellow-150 items-center gap-x-1">
+            <img src="/icons/icon16_man.svg"><h1>Johann Abad</h1>
+        </span>
         <span class="flex justify-start text-xs text-yellow-150 items-center gap-x-1">
             <img src="/icons/icon17_contact.svg"><h1>0902105050</h1>
         </span>
